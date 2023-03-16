ce639f4d164a9617a471f05220c84ebd6827ceee
Jumpway3 <jumpway3@gmail.com>
2023-03-16 20:36:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import openai
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters,CallbackContext
 import configparser
@@ -6,6 +7,10 @@ import redis
 global redis1
 def main():
 # Load your token and create an Updater for your Bot
+# Set OpenAI API Secret Key
+    openai.api_key = "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"
+    MODEL_ID = "text-davinci-003"
+
     config = configparser.ConfigParser()
     config.read('config.ini')
     updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
@@ -14,19 +19,42 @@ def main():
     redis1 = redis.Redis(host=(config['REDIS']['HOST']), password=(config['REDIS']['PASSWORD']), port=(config['REDIS']['REDISPORT']))
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 # register a dispatcher to handle message: here we register an echo dispatcher
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-    dispatcher.add_handler(echo_handler)
+#     echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+#     dispatcher.add_handler(echo_handler)
 # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("add", add))
     dispatcher.add_handler(CommandHandler("help", help_command))
+
 # To start the bot:
     updater.start_polling()
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, handle_message))
     updater.idle()
-def echo(update, context):
-    reply_message = update.message.text.upper()
-    logging.info("Update: " + str(update))
-    logging.info("context: " + str(context))
-    context.bot.send_message(chat_id=update.effective_chat.id, text= reply_message)
+def handle_message(update, context):
+    # Get input text
+    user_input = update.message.text
+
+    # Use
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=user_input,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    # 获取 OpenAI API 返回的生成文本
+    generated_text = response.choices[0].text.strip()
+    # 存储生成的文本到 Redis 数据库中
+    redis1.incr(generated_text)
+    # 向用户发送生成的文本
+    update.message.reply_text(generated_text)
+
+# def echo(update, context):
+#     reply_message = update.message.text.upper()
+#     logging.info("Update: " + str(update))
+#     logging.info("context: " + str(context))
+#     context.bot.send_message(chat_id=update.effective_chat.id, text= reply_message)
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
 def help_command(update: Update, context: CallbackContext) -> None:
