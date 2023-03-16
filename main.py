35cfe892e84f4b86ded65e941b9245526d846cf6
Jumpway3 <jumpway3@gmail.com>
2023-03-16 18:03:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,57 @@
 import os
+
 import telegram
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, updater
 import configparser
-import redis
 import logging
-from flask import Flask, request
-
-app = Flask(__name__)
-
-# Load your token and create a bot instance
-config = configparser.ConfigParser()
-config.read('config.ini')
-bot = telegram.Bot(token=config['TELEGRAM']['ACCESS_TOKEN'])
-redis1 = redis.Redis(host=config['REDIS']['HOST'], password=config['REDIS']['PASSWORD'], port=config['REDIS']['REDISPORT'])
-
-# Handle incoming telegram messages
-@app.route(f"/{config['TELEGRAM']['ACCESS_TOKEN']}", methods=['POST'])
-def handle_telegram_message():
-    # Retrieve the message in JSON format and convert it to Telegram object
-    update = telegram.Update.de_json(request.get_json(force=True), bot)
-    logging.info(f"Received message: {update.message.text}")
-
-    # Echo the message back to the user
-    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
-
-    return 'ok'
-
+import redis
+global redis1
+def main():
+    # Load your token and create an Updater for your Bot
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    dispatcher = updater.dispatcher
+    global redis1
+    redis1 = redis.Redis(host=(config['REDIS']['HOST']), password=(config['REDIS']
+    ['PASSWORD']), port=(config['REDIS']['REDISPORT']))
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+    # register a dispatcher to handle message: here we register an echo dispatcher
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    dispatcher.add_handler(echo_handler)
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("add", add))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+    # To start the bot:
+    updater.start_webhook(
+        listen="0.0.0.0",
+        port=int(os.getenv('PORT')),
+        webhook_url="https://letsnewchat.herokuapp.com/"
+    )
+    updater.idle()
+def echo(update, context):
+    reply_message = update.message.text.upper()
+    logging.info("Update: " + str(update))
+    logging.info("context: " + str(context))
+    context.bot.send_message(chat_id=update.effective_chat.id, text= reply_message)
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
-@app.route('/add')
-def add_keyword():
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Helping you helping you.')
+def add(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /add is issued."""
     try:
-        keyword = request.args.get('keyword')
-        if keyword is None:
-            return 'Keyword not found', 400
-        redis1.incr(keyword)
-        return f"You have said {keyword} for {redis1.get(keyword).decode('UTF-8')} times.", 200
-    except:
-        return 'Error occurred while processing your request', 500
-
-@app.route('/help')
-def help_command():
-    return 'Helping you helping you.', 200
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+        global redis1
+        logging.info(context.args[0])
+        msg = context.args[0] # /add keyword <-- this should store the keyword
+        redis1.incr(msg)
+        update.message.reply_text('You have said ' + msg + ' for ' +
+                                  redis1.get(msg).decode('UTF-8') + ' times.')
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /add <keyword>')
+# 
+# if __name__ == '__main__':
+#     main()
