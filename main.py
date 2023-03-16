24ec026ed93c2a43c27ce51e52d27bec36e1a742
Jumpway3 <jumpway3@gmail.com>
2023-03-16 17:53:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,58 +1,46 @@
 import os
-
 import telegram
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import configparser
-import logging
 import redis
-from flask import Flask, request, abort
+import logging
+from flask import Flask, request
 
 app = Flask(__name__)
 
-global redis1
+# Load your token and create a bot instance
 config = configparser.ConfigParser()
 config.read('config.ini')
-redis1 = redis.Redis(host=config['REDIS']['HOST'],
-                     password=config['REDIS']['PASSWORD'],
-                     port=config['REDIS']['REDISPORT'])
+bot = telegram.Bot(token=config['TELEGRAM']['ACCESS_TOKEN'])
+redis1 = redis.Redis(host=config['REDIS']['HOST'], password=config['REDIS']['PASSWORD'], port=config['REDIS']['REDISPORT'])
 
-def echo(update, context):
-    reply_message = update.message.text.upper()
-    logging.info("Update: " + str(update))
-    logging.info("context: " + str(context))
-    context.bot.send_message(chat_id=update.effective_chat.id, text= reply_message)
-
-def help_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Helping you helping you.')
-
-def add(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /add is issued."""
-    try:
-        global redis1
-        logging.info(context.args[0])
-        msg = context.args[0] # /add keyword <-- this should store the keyword
-        redis1.incr(msg)
-        update.message.reply_text('You have said ' + msg + ' for ' + redis1.get(msg).decode('UTF-8') + ' times.')
-    except (IndexError, ValueError):
-        update.message.reply_text('Usage: /add <keyword>')
-
-@app.route("/")
-def index():
-    return "Hello World!"
-
+# Handle incoming telegram messages
 @app.route(f"/{config['TELEGRAM']['ACCESS_TOKEN']}", methods=['POST'])
-def webhook():
+def handle_telegram_message():
+    # Retrieve the message in JSON format and convert it to Telegram object
     update = telegram.Update.de_json(request.get_json(force=True), bot)
-    dp.process_update(update)
-    return 'OK'
+    logging.info(f"Received message: {update.message.text}")
+
+    # Echo the message back to the user
+    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+
+    return 'ok'
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+@app.route('/add')
+def add_keyword():
+    try:
+        keyword = request.args.get('keyword')
+        if keyword is None:
+            return 'Keyword not found', 400
+        redis1.incr(keyword)
+        return f"You have said {keyword} for {redis1.get(keyword).decode('UTF-8')} times.", 200
+    except:
+        return 'Error occurred while processing your request', 500
+
+@app.route('/help')
+def help_command():
+    return 'Helping you helping you.', 200
 
 if __name__ == '__main__':
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-    bot = telegram.Bot(token=config['TELEGRAM']['ACCESS_TOKEN'])
-    dp = telegram.ext.Dispatcher(bot, None)
-    dp.add_handler(MessageHandler(Filters.text & (~Filters.command), echo))
-    dp.add_handler(CommandHandler("add", add))
-    dp.add_handler(CommandHandler("help", help_command))
-    app.run(port=int(os.environ.get('PORT', 5000)), debug=True, use_reloader=False)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
