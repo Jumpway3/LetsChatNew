04c9e09ad0a959dc67ccc51fb88d43cb30b46f85
Jumpway3 <jumpway3@gmail.com>
2023-04-06 22:03:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,30 @@
 import os
-
+import mysql.connector
 import openai
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters,CallbackContext
 import configparser
 import logging
-import redis
-global redis1
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
+
 def main():
-# Load your token and create an Updater for your Bot
-# Set OpenAI API Secret Key
-    openai.api_key = os.environ['OPENAI_TOKEN']
-    MODEL_ID = "text-davinci-003"
-
-    # config = configparser.ConfigParser()
-    # config.read('config.ini')
-    updater = Updater(token=(os.environ['ACCESS_TOKEN']), use_context=True)
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    openai_token = config.get('OPENAI', 'OPENAI_API_KEY')
+    openai.api_key = openai_token
+    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
-    global redis1
-    redis1 = redis.Redis(host=(os.environ['HOST']), password=(os.environ['PASSWORD']), port=(os.environ['REDISPORT']))
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-# register a dispatcher to handle message: here we register an echo dispatcher
-#     echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-#     dispatcher.add_handler(echo_handler)
-# on different commands - answer in Telegram
-    dispatcher.add_handler(CommandHandler("add", add))
-    dispatcher.add_handler(CommandHandler("help", help_command))
-
-# To start the bot:
+    # To start the bot:
     updater.start_polling()
     updater.dispatcher.add_handler(MessageHandler(Filters.text, handle_message))
     updater.idle()
+
 def handle_message(update, context):
     # Get input text
     user_input = update.message.text
@@ -46,33 +39,27 @@ def handle_message(update, context):
         temperature=0.5,
     )
 
-    # 获取 OpenAI API 返回的生成文本
+
     generated_text = response.choices[0].text.strip()
-    # 存储生成的文本到 Redis 数据库中
-    redis1.incr(generated_text)
-    # 向用户发送生成的文本
+
     update.message.reply_text(generated_text)
 
-# def echo(update, context):
-#     reply_message = update.message.text.upper()
-#     logging.info("Update: " + str(update))
-#     logging.info("context: " + str(context))
-#     context.bot.send_message(chat_id=update.effective_chat.id, text= reply_message)
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
-def help_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Helping you helping you.')
-def add(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /add is issued."""
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    connection = mysql.connector.connect(
+        host = config.get('DATABASE','host'),
+        user = config.get('DATABASE','user'),
+        password = config.get('DATABASE','password'),
+        database = config.get('DATABASE','database')
+
+    )
+
     try:
-        global redis1
-        logging.info(context.args[0])
-        msg = context.args[0] # /add keyword <-- this should store the keyword
-        redis1.incr(msg)
-        update.message.reply_text('You have said ' + msg + ' for ' +
-                          redis1.get(msg).decode('UTF-8') + ' times.')
-    except (IndexError, ValueError):
-        update.message.reply_text('Usage: /add <keyword>')
+        with connection.cursor() as cursor:
+            insert_query = "INSERT INTO generated_text (text) VALUES (%s)"
+            cursor.execute(insert_query, (generated_text,))
+            connection.commit()
+    finally:
+        connection.close()
 if __name__ == '__main__':
     main()
